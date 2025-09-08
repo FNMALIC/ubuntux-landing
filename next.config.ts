274ed442b60685f521c8e795d6efde06e4b115ea
FNMALIC <fonkounixon@gmail.com>
2025-09-08 15:10:54 +0100

    update next config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // Required for static exports
   },
+    reactStrictMode: true,
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    output: "standalone",
+    trailingSlash: false,
   /* config options here */
 };
 
